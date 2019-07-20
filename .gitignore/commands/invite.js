614f45ca12757020cc6e7164fa47533ab5cef031
AKX-Discord <49238921+KxbabVII@users.noreply.github.com>
2019-07-20 22:16:45 +0200

    Update invite.js

--- .gitignore/commands/invite.js
+++ .gitignore/commands/invite.js
@@ -3,7 +3,8 @@ const Discord = require ("discord.js");
 module.exports.run = async (bot, message, args) => {
     let botIcon = bot.user.displayAvatarURL;
     let inviteEmbed = new Discord.RichEmbed()
-        .setDescription("Lien du serveur support : https://discord.gg/4gfWGEj", "Lien d'invitation du BOT : https://discordapp.com/oauth2/authorize?client_id=573126775673061377&scope=bot&permissions=8")
+        .setTitle("Lien du serveur support : https://discord.gg/4gfWGEj")
+        .setAuthor("Lien d'invitation du BOT : https://discordapp.com/oauth2/authorize?client_id=573126775673061377&scope=bot&permissions=8")
         .setColor("RANDOM")
         .addBlankField()
         .addBlankField()
