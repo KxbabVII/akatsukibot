2a83b44a88764fe0a1c8ee2abb8edb2466726a82
AKX-Discord <49238921+KxbabVII@users.noreply.github.com>
2019-07-20 21:59:30 +0200

    Create invite.js

---
+++ .gitignore/commands/invite.js
@@ -0,0 +1,20 @@
+const Discord = require ("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let botIcon = bot.user.displayAvatarURL;
+    let inviteEmbed = new Discord.RichEmbed()
+        .setTitle("Lien du serveur support : https://discord.gg/4gfWGEj")
+        .setDescription("Lien d'invitation du BOT : https://discordapp.com/oauth2/authorize?client_id=573126775673061377&scope=bot&permissions=8")
+        .setColor("RANDOM")
+        .addBlankField()
+        .addBlankField()
+        .setFooter(`Akatsuki | Commande invite demandé par ${message.author.tag}`, botIcon)
+        .setThumbnail(`${message.author.avatarURL}`);
+
+        return message.channel.send(inviteEmbed)
+        
+  }
+
+module.exports.help = {
+    name: "invite"
+};
