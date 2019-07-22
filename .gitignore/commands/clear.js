1fb4f7eb9d6c66c21c4efbe71c5222339a8f26d3
AKX-Discord <49238921+KxbabVII@users.noreply.github.com>
2019-07-22 11:21:19 +0200

    Update clear.js

--- .gitignore/commands/clear.js
+++ .gitignore/commands/clear.js
@@ -1,7 +1,28 @@
 const Discord = require ("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let botIcon = bot.user.displayAvatarURL
+ 
+    if(!message.member.hasPermission("MANAGE_MESSAGES"))
+        let errorPermClear = new Discord.RichEmbed()
+        .setDescription("Vous n'avez pas la permission d'utiliser cette commande.")
+        .setColor("RANDOM")
+         message.reply(errorPermClear)
+
+    if (!args[0])
+    let argsErrorClear = new Discord.RichEmbed()
+    .setDescription("Veuillez indiquer le nombre de message à supprimer | Syntaxe : ?clear nombre")
+    .setColor("RANDOM")
+
+    let clearEmbed = new Discord.RichEmbed()
+    .setDescription(`Suppression de **${args[0]}** messages en cours.`)
+    .setColor("RANDOM")
+
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel
+        .send(clearEmbed)
+        .then(message => message.delete(5000));
+
+    })
 
     }
 
