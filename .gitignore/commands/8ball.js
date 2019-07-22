4a0b0f4a3571f2451b981d59a2cca866fb229bed
AKX-Discord <49238921+KxbabVII@users.noreply.github.com>
2019-07-22 13:04:39 +0200

    Update 8ball.js

--- .gitignore/commands/8ball.js
+++ .gitignore/commands/8ball.js
@@ -8,12 +8,13 @@ module.exports.run = async (bot, message, args) => {
    .setColor("RANDOM")
     if (!args[0]) return message.reply(askErrorEmbed)
 
-    let replies = ["oui.", "non.", "certainement.", "jaaj !", "pourquoi pas.", "tais toi.", "flemme de te répondre.", "hé oui Jamy !", "tu veux du pâté ?", "certainement.", "bien vu."]
+    let replies = ["oui.", "non.", "certainement.", "jaaj.", "pourquoi pas.", "tais toi.", "flemme de te répondre.", "hé oui Jamy !", "tu veux du pâté ?", "certainement.", "bien vu.", "pour ne pas te lacher un nique ta mère.", "je ne sais pas.",
+    "sans pression.", "tu dis que de la merde.", "tu pue.", "sah quel plaisir.", "couillon va.", "jvounike"]
     let question = args.slice(0).join(" ");
     let res = Math.floor((Math.random() * replies.length));
 
     let askEmbed = new Discord.RichEmbed()
-    .setColor("#cb4b16")
+    .setColor("RANDOM")
     .setFooter(`Akatsuki | Commande 8ball demandé par ${message.author.tag}`, botIcon)
     .setAuthor(`Question posé : ${question}`)
     .setDescription(`Réponse du bot : ${replies[res]}`);
