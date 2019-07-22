87eefcebf8e2e3ea8ddf5037ba8bdfaa89d07ed4
AKX-Discord <49238921+KxbabVII@users.noreply.github.com>
2019-07-22 10:50:40 +0200

    Update purge.js

--- .gitignore/commands/purge.js
+++ .gitignore/commands/purge.js
@@ -3,7 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 if (message.deletable) message.delete();
-
+    let errorPermissionPurge = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setDescription("Vous n'avez pas la permission d'utiliser cette commande.")
+if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(errorPermissionPurge)
     message.channel.fetchMessages().then((message) => message.forEach(m => m.delete()));
 
     let purgeEmbed = new Discord.RichEmbed()
