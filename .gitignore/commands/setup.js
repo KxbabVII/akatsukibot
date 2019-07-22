e4e9d5889bce00e39dce2458eecb324ac8812238
AKX-Discord <49238921+KxbabVII@users.noreply.github.com>
2019-07-22 11:27:48 +0200

    Create setup.js

---
+++ .gitignore/commands/setup.js
@@ -0,0 +1,24 @@
+const Discord = require ("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.hasPermission("ADMINISTRATOR"))
+    let errorPermSetup = new Discord.RichEmbed()
+    .setDescription("Vous n'avez pas la permission d'utiliser cette commande.")
+    .setColor("RANDOM")
+     message.reply(errorPermSetup)
+
+     message.guild.createChannel('「🖥」report',"text")
+     message.guild.createChannel('「📔」logs-akatsuki',"text")
+     
+     let setupEmbed = new Discord.RichEmbed()
+     .setDescription("Installation du setup de Akatsuki finie avec succès.")
+     .setColor("RANDOM")
+
+     message.channel.send(setupEmbed)
+
+}
+
+module.exports.help = {
+    name: "help"
+};
